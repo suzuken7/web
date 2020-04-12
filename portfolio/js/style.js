99406872f9de05271a40a7a015992ab7cca73519
suzuken <no mail>
2020-04-12 23:54:12 +0900

    worksアニメーション

--- portfolio/js/style.js
+++ portfolio/js/style.js
@@ -161,3 +161,18 @@ jQuery(function() {
         return false;
     });
 });
+
+// 下からフェードイン
+$(function(){
+    $(window).scroll(function (){
+        $('.fadein').each(function(){
+            var targetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
+    });
+});
