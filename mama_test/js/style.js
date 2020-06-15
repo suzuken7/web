ddbe6d8765c1664752072af9cb4ecee94d13921d
suzuken <no mail>
2020-06-15 21:59:30 +0900

    voice部分対応

--- mama_test/js/style.js
+++ mama_test/js/style.js
@@ -67,3 +67,19 @@ function ChangeImg(url,e) {
     e.classList.add('active');
 }
 
+
+/*------------------------------------------*
+* パララックス
+*------------------------------------------*/
+$(function() {
+	var top = $('#box1').offset().top; //初期値を取得
+	$(window).scroll(function() {
+	    var value = $(this).scrollTop(); //スクロールの値を取得
+	    $('#scrollValue').text(value);
+ 
+	    $('#box1').css('top', top + value / 2);
+	    $('#box2').css('top', top + value / 4);
+	    $('#box3').css('top', top + value / 6);
+	    $('#box4').css('top', top + value / 8);
+	});
+});
