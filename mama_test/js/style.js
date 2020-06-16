09c91eaf02ebed79cd441bc447df675bf3172593
suzuken <no mail>
2020-06-16 10:38:59 +0900

    会社概要テーブル部分の対応

--- mama_test/js/style.js
+++ mama_test/js/style.js
@@ -69,17 +69,9 @@ function ChangeImg(url,e) {
 
 
 /*------------------------------------------*
-* パララックス
+* 均等割り付け
 *------------------------------------------*/
-$(function() {
-	var top = $('#box1').offset().top; //初期値を取得
-	$(window).scroll(function() {
-	    var value = $(this).scrollTop(); //スクロールの値を取得
-	    $('#scrollValue').text(value);
- 
-	    $('#box1').css('top', top + value / 2);
-	    $('#box2').css('top', top + value / 4);
-	    $('#box3').css('top', top + value / 6);
-	    $('#box4').css('top', top + value / 8);
-	});
+$(function(){
+    $('.justify').justify();
 });
+    
