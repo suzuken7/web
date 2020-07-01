d9952541ece36e74619ecbdccb9c7c98eed35fe7
suzuken <no mail>
2020-07-02 00:14:07 +0900

    更新

---
+++ old/mama_test/js/style.js
@@ -0,0 +1,194 @@
+/*------------------------------------------*
+* 定数
+*------------------------------------------*/
+var winW = 768;     // スマホブレイクポイント_
+
+
+/*------------------------------------------*
+* メインヴィジュアルフェード処理
+*------------------------------------------*/
+$(function() {
+    $('#slides').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        arrows: false,
+    });
+});
+
+
+/*------------------------------------------*
+* ナビゲーションバー固定処理
+*------------------------------------------*/
+$( window ).on( 'scroll', function() {
+    var winH = 550;     // メインビジュアルのサイズ_
+
+    // pcとタブレットのみ対応_
+    if ( winH < jQuery( this ).scrollTop() && winW < window.innerWidth ) {
+        jQuery( 'body' ).css( 'padding-top', 100);
+        jQuery( '#gNav' ).addClass( 'fixed' );
+    } else {
+        jQuery( 'body' ).css( 'padding-top', 0 );
+        jQuery( '#gNav' ).removeClass( 'fixed' );
+    }
+});
+
+
+/*------------------------------------------*
+* ハンバーガーメニュー切り替え処理
+*------------------------------------------*/
+$(function(){
+    $(".gNav__wrapper__btn").on("click", function(){
+        var rightVal = 0;
+
+        if($(this).hasClass("open")) {
+            // 位置を移動させメニューを開いた状態にする
+            rightVal = -300;
+            $(this).removeClass("open");
+        } else {
+            $(this).addClass("open");
+        }
+ 
+        $("#gNav__menu").stop().animate({
+            right: rightVal
+        }, 200);
+    });
+});
+
+
+/*------------------------------------------*
+* スムーススクロール
+* ナビメニュータップ時のアニメーション
+*------------------------------------------*/
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var navOffset = 0;
+        if ( winW < window.innerWidth ) {
+            navOffset = 92;     // ナビバー用の調整値_
+        }
+
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - navOffset;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
+
+
+/*------------------------------------------*
+* 特徴タブ切り替え処理
+*------------------------------------------*/
+$(function() {
+    $('.feature__tab').click(function() {
+        // 変更した文字色をリセット_
+        $('.feature__tab.is-active').css({
+            'color': '#ffffff'
+        });
+
+        $('.is-active').removeClass('is-active');
+        $(this).addClass('is-active');
+        $('.is-show').removeClass('is-show');
+        // クリックしたタブからインデックス番号を取得
+        const index = $(this).index();
+        // クリックしたタブと同じインデックス番号をもつコンテンツを表示
+        $('.feature__panel').eq(index).addClass('is-show');
+
+        jQuery( '.feature__panel_group' ).css( 'height', 400 );
+
+        switch (index) {
+            case 0: // 特徴1
+                ChangeBorderColor('#ff9000');
+                break;
+            case 1: // 特徴2
+                ChangeBorderColor('#3ebc01');
+                break;
+            case 2: // 特徴3
+                ChangeBorderColor('#1e1efc');
+                jQuery( '.feature__panel_group' ).css( 'height', "auto");
+                break;
+            case 3: // 特徴4
+                ChangeBorderColor('#fc1e77');
+
+                break;
+        } 
+    });
+});
+
+// ボーダーと文字の色変え用_
+function ChangeBorderColor (colorCode) {
+    $('.feature__panel_group').css({
+        'border-color' : colorCode
+    }); 
+    
+    // 「特徴」の文字色を変更_
+    $('.feature__tab.is-active').css({
+        'color': colorCode
+    });
+}
+
+
+/*------------------------------------------*
+* コンセプト画像切り替え処理
+*------------------------------------------*/
+function ChangeImg(url,e) {
+    document.getElementById("img").src = url;
+    let nodes = document.getElementById("thumb_img");
+    let img_child = nodes.children;
+    for (i = 0; i < img_child.length; i++) {
+        img_child[i].classList.remove('active')
+    }
+    e.classList.add('active');
+}
+
+
+/*------------------------------------------*
+* 均等割り付け
+*------------------------------------------*/
+$(function(){
+    $('.justify').justify();
+});
+    
+
+/*------------------------------------------*
+* パララックス処理
+*------------------------------------------*/
+$(function(){
+    
+    let scrollPosi = 0;
+
+    $(window).scroll(function () {
+        scrollPosi = $(document).scrollTop() - 300;
+        
+        $('#parallax').stop(true, true).animate({
+            'background-position-y': -scrollPosi / 5 + 'px'
+        }, 100);
+    });
+ 
+});
+
+
+/*------------------------------------------*
+* トップ矢印フェード処理
+* 100px進んだらふわっと浮き上がる
+*------------------------------------------*/
+$(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500); //0.5秒かけてトップへ移動
+        return false;
+    });
+});
--- portfolio_wp/js/style.js
+++ portfolio_wp/js/style.js
@@ -20,3 +20,22 @@ $(function(){
 });
 
 
+/*------------------------------------------*
+* スムーススクロール
+* ナビメニュータップ時のアニメーション
+*------------------------------------------*/
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var navOffset = 280;
+        // if ( winW < window.innerWidth ) {
+        //     navOffset = 92;     // ナビバー用の調整値_
+        // }
+
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - navOffset;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
